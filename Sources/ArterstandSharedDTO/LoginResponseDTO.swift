4df1b17efc5b763b88ab1d4923a1918641da26a3
Dosso <massidedosso@gmail.com>
2024-11-11 23:24:48 +0100

    Creation: -LoginResponseDTO -RegisterResponseDTO

--- Sources/ArterstandSharedDTO/LoginResponseDTO.swift
+++ Sources/ArterstandSharedDTO/LoginResponseDTO.swift
@@ -1,8 +1,24 @@
 //
-//  File.swift
+//  LoginResponseDTO.swift
 //  
 //
 //  Created by Massidé Dosso on 11/11/2024.
 //
 
 import Foundation
+
+public struct LoginResponseDTO: Codable {
+    
+    public let error: Bool
+    public var reason: String? = nil
+    public var token: String? = nil
+    public var artistID: UUID? = nil
+    
+    public init(error: Bool, reason: String? = nil, token: String? = nil, artistID: UUID? = nil) {
+        self.error = error
+        self.reason = reason
+        self.token = token
+        self.artistID = artistID
+    }
+}
+
