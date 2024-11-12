cce4a1643b0f129af315c721eedbe5ebe7e883dd
Dosso <massidedosso@gmail.com>
2024-11-12 09:11:36 +0100

    artistID -> userID in Login ResponseDTO file

--- Sources/ArterstandSharedDTO/LoginResponseDTO.swift
+++ Sources/ArterstandSharedDTO/LoginResponseDTO.swift
@@ -12,13 +12,13 @@ public struct LoginResponseDTO: Codable {
     public let error: Bool
     public var reason: String? = nil
     public var token: String? = nil
-    public var artistID: UUID? = nil
+    public var userID: UUID? = nil
     
-    public init(error: Bool, reason: String? = nil, token: String? = nil, artistID: UUID? = nil) {
+    public init(error: Bool, reason: String? = nil, token: String? = nil, userID: UUID? = nil) {
         self.error = error
         self.reason = reason
         self.token = token
-        self.artistID = artistID
+        self.userID = userID
     }
 }
 
